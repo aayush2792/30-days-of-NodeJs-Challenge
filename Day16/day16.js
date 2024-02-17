a588f65fe382f17e847ea5c50337204f7f58b667
Spanchiken <103322928+aayush2792@users.noreply.github.com>
2024-02-17 17:06:51 +0530

    MongoDB connection setup

--- Day16/day16.js
+++ Day16/day16.js
@@ -1,30 +1,14 @@
-const express = require('express');
 const mongoose = require('mongoose');
 
-const app = express();
+function connectToMongoDB(){
+    const mongo = 'mongodb://localhost/myDatabase';
+    mongoose.connect(mongo);
+    mongoose.connection.once('open', () => {
+        console.log('Connected');
+    });
+    mongoose.connection.on('error', ()=> {
+        console.log('Error');
+    });
+}
 
-// Replace with your actual MongoDB connection string
-const MONGO_URI = 'mongodb://your_mongodb_server_ip:27017/cluster0';
-
-
-// Function to connect to the MongoDB database
-const connectToMongoDB = async () => {
-    try {
-        mongoose.connect(MONGO_URI);
-        console.log('Successfully connected to MongoDB database!');
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error.message);
-        process.exit(1); // Exit the application if connection fails
-    }
-};
-
-// Connect to MongoDB before starting the server
 connectToMongoDB();
-
-// Your other Express application code here...
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
